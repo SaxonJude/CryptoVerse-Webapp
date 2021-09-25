152711accc236bb7e8d357aff7bd584869af39b8
SaxonJude <saxonjude@gmail.com>
2021-09-25 16:32:15 +0100

    Added Routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOUtlined, MoneyCollectedOutlined, BulbOutlined, FundOutLined, ManuOutlined} from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
 
 import icon from '../images/cryptocurrency.png';
 
@@ -14,6 +14,20 @@ const Navbar = () => {
                     <Link to='/'>Cryptoverse</Link>
                 </Typography.Title >
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
