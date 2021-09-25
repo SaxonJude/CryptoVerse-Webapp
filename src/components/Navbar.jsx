f72c9a73a997ff792d5e03d37be826271c08a5b3
SaxonJude <saxonjude@gmail.com>
2021-09-25 15:04:18 +0100

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOUtlined, MoneyCollectedOutlined, BulbOutlined, FundOutLined, ManuOutlined} from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <div className='logo-container'>
+                <Avatar />
+                <Typography.Title level={2} className='logo'>
+                    <Link to='/'>Cryptoverse</Link>
+                </Typography.Title >
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
